f4b31e99e31791a9d52b78cdd98b59e687a6487d
Farjana <farjanajhn12@gmail.com>
2023-05-05 16:37:05 +0600

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = 3000
 
-const chefsInfo = require('./data/chefsInfo.json');
+const chefsInfos = require('./data/chefsInfo.json');
+const recipes=require('./data/recipes.json')
 
 app.use(cors())
 
@@ -11,10 +12,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/chefsInfo', (req, res) => {
-  res.send(chefsInfo);
+app.get('/chefsInfos', (req, res) => {
+  res.send(chefsInfos);
 })
 
-app.listen(port, () => {
+app.get('/recipes', (req, res) => {
+  res.send(recipes);
+})
+
+app.get('/recipes/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedRecipes = recipes.find(r => r._id === id);
+  res.send(selectedRecipes);
+});
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const categoryRecipes = recipes.filter(r => r.category_id === id);
+  res.send(categoryRecipes)
+});
+ 
+
+
+app.listen(port,()  => {
   console.log(`Example app listening on port ${port}`)
 })
