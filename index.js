5b5489a170051ac793d2292b8d3dc87690013d9c
Farjana <farjanajhn12@gmail.com>
2023-06-04 12:18:10 +0600

    some changes in json file and port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const port = 3000
+const port = 8080
 
 const chefsInfos = require('./data/chefsInfo.json');
 const recipes=require('./data/recipes.json')
